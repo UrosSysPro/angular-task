928596dfa4fdc4c28011126ef7d690eb091b1efd
Uros Karaleic <karaleicuu@gmail.com>
2025-06-29 22:13:08 +0200

    remove old state management

--- src/app/components/app/app.ts
+++ src/app/components/app/app.ts
@@ -23,8 +23,6 @@ export class NormalState implements AppState{
   constructor(public entries:TimeEntry[]){}
 }
 
-
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, EmployeePieChart, EmployeeTable],
@@ -54,54 +52,8 @@ export class App implements OnInit {
       return Array.from(map.values()).sort((a,b)=>a.TotalHoursInMonth<b.TotalHoursInMonth?1:-1);
     }else return [] as TimeEntryPerUser[];
   });
-  isLoading=signal(true)
-  subscription=signal<Subscription|null>(null);
-  timeEntries=signal<TimeEntry[]|null>(null);
-  hasErrorOccurred=signal(false);
-
-  handleDtoEntries(dtoEntries :TimeEntryDto[]) {
-    const  entries = dtoEntries.map(timeEntryDtoToModel);
-    const map:Map<string,TimeEntry>=new Map();
-    for(const entry of entries){
-      if(entry.employeeName==null)continue;
-      if(map.has(entry.employeeName)){
-        const previous=map.get(entry.employeeName)!;
-        map.set(entry.employeeName, {
-          ...previous,
-          totalHoursInMonth:previous.totalHoursInMonth+entry.totalHoursInMonth,
-        });
-      }else{
-        map.set(entry.employeeName, entry);
-      }
-    }
-    const compactedEntries=Array.from(map.values());
-    const sortedEntries=compactedEntries.sort((a,b)=>a.totalHoursInMonth<b.totalHoursInMonth?1:-1);
-    this.timeEntries.set(sortedEntries);
-    this.isLoading.set(false);
-  }
-
-  handleError(error:any){
-    console.log(error);
-    this.hasErrorOccurred.set(true);
-    this.isLoading.set(false);
-  }
 
   requestData(){
-    // reset app state
-    // this.isLoading.set(true);
-    // this.hasErrorOccurred.set(false);
-    // this.timeEntries.set(null);
-    // this.subscription()?.unsubscribe();
-    //
-    // // make and handle request
-    // const observer = this.azureWebsiteService.getMeetingEntries(this.code)
-    // this.subscription.set(observer.pipe(catchError((error,observable) => {
-    //   this.handleError(error);
-    //   return of([]);
-    // })).subscribe(dtoEntries => {
-    //   this.handleDtoEntries(dtoEntries);
-    // }));
-
     //reset appState
     this.appState.set(LoadingState);
 
