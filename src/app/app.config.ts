d52b73f6fea3a9f72067fefbff33481b9d4564f7
Uros Karaleic <karaleicuu@gmail.com>
2025-06-30 01:23:56 +0200

    add jsonp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideHttpClient, withFetch, withJsonpSupport} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       // withFetch(),
+      withJsonpSupport()
     ),
   ]
 };
